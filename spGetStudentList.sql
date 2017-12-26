03fd7e73574c7ff2dc5f99fac5acb6cff8890c42
Bonnie S <bonnie_sterling@hotmail.com>
2017-12-26 10:52:28 -0500

    Add files via upload
    stored procedure used in web service spGetStudentList

---
+++ spGetStudentList.sql
@@ -0,0 +1,20 @@
+/**  create a test table and fill it with students
+create table dbo.tblStudents
+(
+  StudentID int primary key identity,
+  StudentName varchar(20),
+  StudentGrade varchar(20),
+  StudentTotalMarks int
+)
+*****/
+
+/*** create a stored procedure called spGetStudentList which returns a the list of students via ADO to a webservice ***/
+create procedure spGetStudentList
+@ID int
+AS
+BEGIN
+   SELECT * FROM dbo.tblStudents
+   WHERE StudentID = @ID
+END
+
+spGetStudentList 10
